ed8d1960b3103ef1bdc0b8a3a6a5f5c8d3dcba7a
Keshav Nangare <knangare@vmware.com>
2022-03-12 12:23:13 +0530

    Git cherry pick command

--- java/Test.java
+++ java/Test.java
@@ -2,5 +2,6 @@ class Test {
     public static void main(String[] args) {
         System.out.println("Welcome to git demo!"); 
         System.out.println("Created new dev branch"); 
+        System.out.println("Git cherry-pick"); 
     }
 }
