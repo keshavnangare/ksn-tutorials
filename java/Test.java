9a70796db41a58c13b0646142f55d7e2c8094ee5
Keshav Nangare <knangare@vmware.com>
2022-03-12 12:25:14 +0530

    Compare dev and master

--- java/Test.java
+++ java/Test.java
@@ -3,5 +3,6 @@ class Test {
         System.out.println("Welcome to git demo!"); 
         System.out.println("Created new dev branch"); 
         System.out.println("Git cherry-pick"); 
+        System.out.println("Comparing dev and master"); 
     }
 }
